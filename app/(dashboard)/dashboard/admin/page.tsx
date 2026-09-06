79418c75b21d89283943ce0fdb21083c6a338890
0jann-exe <r4zan.maulana@gmail.com>
2026-09-06 08:43:51 +0700

    dynamic rendering buat admin

--- app/(dashboard)/dashboard/admin/page.tsx
+++ app/(dashboard)/dashboard/admin/page.tsx
@@ -1,4 +1,5 @@
 'use client';
+export const dynamic = 'force-dynamic';
 
 import React, { useMemo, useState, useEffect } from 'react';
 import { createClient } from '@/app/utils/supabase/client'; // Sesuaikan path helper supabase client Anda
